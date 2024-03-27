9733aecbbda8c25613a9ba0bc390f0faa177e38f
roberta7724 <roberta.bara@emanuel.ro>
2024-03-27 21:58:50 +0200

    tema 6

---
+++ tema 6/script.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(800,500);
+}
+function draw() {
+    background ("blue");
+    fill("rgba(255, 226, 148, 0.981)")
+    square(270,200,300);
+    fill("brown")
+    triangle(270,200,420,100,570,200);
+    fill("aqua")
+    square(300,250,80);
+    fill("green")
+    rect(420,280,100,220);
+    
+}
