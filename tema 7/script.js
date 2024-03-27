fd6b859b4b17b7ca4b604c1aa5f6fbeb23d5ac71
roberta7724 <roberta.bara@emanuel.ro>
2024-03-27 23:13:06 +0200

    tema 7

---
+++ tema 7/script.js
@@ -0,0 +1,29 @@
+function setup() {
+    createCanvas(1500,750);
+}
+function draw() {
+    background("blue");
+fill("blue");
+    ellipse(750,375,1200,600);
+ellipse(750,375,1000,450);
+ellipse(750,375,800,350);
+ellipse(750,375,600,250);
+fill("yellow");
+circle(750,375,80);
+triangle(733,333,767,333,750,297)
+triangle(733,417,767,417,750,453)
+triangle(722,340,705,365,680,330)
+triangle(778,340,795,365,820,330)
+triangle(778,410,795,385,820,420)
+triangle(722,410,705,385,680,420)
+fill("green")
+circle(1300,275,80);
+fill("purple")
+circle(1000,250,100);
+fill("red")
+circle(400,550,50);
+fill("orange")
+circle(500,320,80);
+
+
+}
